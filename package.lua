678b55bfe2ce1697e7c1eaefce6b0ff653edd3ac
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-13 17:12:38 -0500

    bump to 0.2.1

--- package.lua
+++ package.lua
@@ -1,13 +1,12 @@
 return {
   name = "rackspace-monitoring-client",
-  version = "0.2.0",
+  version = "0.2.1",
   description = "Rackspace Monitoring Client",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
-    "luvit/luvit@2.0.0",
+    "luvit/luvit@2.0.1",
     "rphillips/async@0.0.2",
-    "virgo-agent-toolkit/request@0.2.0",
-    "virgo-agent-toolkit/keystone-client@0.2.0",
+    "virgo-agent-toolkit/keystone-client@0.2.1",
   },
   files = {
     "**.lua"
