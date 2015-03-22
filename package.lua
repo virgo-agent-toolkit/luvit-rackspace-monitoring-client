b6fc824a15ffa93c713fe054b07cd0757e2a821b
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-21 22:16:51 -0500

    bump luvit and keystone client

--- package.lua
+++ package.lua
@@ -4,9 +4,9 @@ return {
   description = "Rackspace Monitoring Client",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
-    "luvit/luvit@2.0.1",
+    "luvit/luvit@2.0.2",
     "rphillips/async@0.0.2",
-    "virgo-agent-toolkit/keystone-client@0.3.0",
+    "virgo-agent-toolkit/keystone-client@0.3.1",
   },
   files = {
     "**.lua"
